c38ebb68677087f97c6737a08138a7013e103d0e
admin <admin@DESKTOP-JV882QA>
2020-02-20 19:57:54 +0800

    1

--- src/Captain.CO2NET/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Captain.CO2NET
-{
-    public class Class1
-    {
-    }
-}
---
+++ src/Captain.CO2NET/Helpers/FileHelper.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+
+namespace Captain.CO2NET.Helpers
+{
+    public class FileHelper
+    {
+        /// <summary>
+        /// 根据完整文件路径获取文件流
+        /// </summary>
+        /// <param name="fullFilePath"></param>
+        /// <returns></returns>
+        public static FileStream GetFileStream(string fullFilePath)
+        {
+            FileStream fileStream = null;
+            if (!string.IsNullOrEmpty(fullFilePath) && File.Exists(fullFilePath))
+            {
+                fileStream = new FileStream(fullFilePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            }
+            return fileStream;
+        }
+    }
+}
