2a8aa68b8321839118f276fc49d4e7c98beb5e6e
Captain <961588918@qq.com>
2020-02-20 17:14:34 +0800

    1

---
+++ src/Captain.CO2NET/Interfaces/ITreeNode.cs
@@ -0,0 +1,17 @@
+﻿namespace Captain.CO2NET.Interfaces
+{
+    /// <summary>
+    /// 树节点
+    /// </summary>
+    public interface ITreeNode
+    {
+        /// <summary>
+        /// 节点ID
+        /// </summary>
+        string Id { get; set; }
+        /// <summary>
+        /// 父节点ID
+        /// </summary>
+        string ParentId { get; set; }
+    }
+}
