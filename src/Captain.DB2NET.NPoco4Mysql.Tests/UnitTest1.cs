c4d3bb5d1194e1dbb3fc7a5fca07e8fb13b1a942
961588918 <961588918@qq.com>
2020-03-22 15:01:07 +0800

    1

---
+++ src/Captain.DB2NET.NPoco4Mysql.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Captain.DB2NET.NPoco4Mysql.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
